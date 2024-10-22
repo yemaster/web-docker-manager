71ca147b934152d5eb3ca7a9dbd9a9f9349c8c5b
taoky <me@taoky.moe>
2024-10-23 01:43:30 +0800

    Create 2 examples: bare flask and uwsgi+gevent flask

---
+++ example-prodflask/app/main.py
@@ -0,0 +1,27 @@
+from flask import (
+    Flask,
+    request,
+    render_template,
+    make_response,
+)
+
+app = Flask(__name__)
+
+f = open("/flag")
+gflag = f.read()
+f.close()
+
+
+@app.route("/", methods=["GET"])
+def index():
+    response = make_response(render_template("index.html"))
+    return response
+
+
+@app.route("/test", methods=["POST"])
+def testpost():
+    namev = request.form["name"]
+    return "Hi %s: Your flag is %s" % (namev, gflag)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
